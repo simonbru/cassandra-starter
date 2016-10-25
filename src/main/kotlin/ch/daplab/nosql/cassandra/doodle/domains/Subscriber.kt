a5b970a5764169eba613c75e594e068449998d24
Simon Brulhart <simon@brulhart.me>
2016-10-25 20:51:26 +0200

    Implement createPoll and getPollById

--- src/main/kotlin/ch/daplab/nosql/cassandra/doodle/domains/Subscriber.kt
+++ src/main/kotlin/ch/daplab/nosql/cassandra/doodle/domains/Subscriber.kt
@@ -1,9 +1,9 @@
 package ch.daplab.nosql.cassandra.doodle.domains
 
-class Subscriber {
-
-    var label: String? = null
+class Subscriber(
+    var label: String? = null,
     var choices: List<String>? = null
+) {
 
     override fun toString(): String {
         return "Subscriber [label=$label, choices=$choices]"
