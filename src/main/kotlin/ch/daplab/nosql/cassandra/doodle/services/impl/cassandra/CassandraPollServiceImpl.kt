ec92669a75b2ac73d2d008c84b3a976ba16e9068
Simon Brulhart <simon@brulhart.me>
2016-10-25 22:26:40 +0200

    Finish implementing PollService

--- src/main/kotlin/ch/daplab/nosql/cassandra/doodle/services/impl/cassandra/CassandraPollServiceImpl.kt
+++ src/main/kotlin/ch/daplab/nosql/cassandra/doodle/services/impl/cassandra/CassandraPollServiceImpl.kt
@@ -3,33 +3,25 @@ package ch.daplab.nosql.cassandra.doodle.services.impl.cassandra
 import ch.daplab.nosql.cassandra.doodle.domains.Poll
 import ch.daplab.nosql.cassandra.doodle.domains.Subscriber
 import ch.daplab.nosql.cassandra.doodle.services.PollService
+import com.datastax.driver.core.Row
 import com.datastax.driver.core.Session
 import com.datastax.driver.core.UDTValue
+import com.datastax.driver.core.UserType
 import java.util.*
 
 class CassandraPollServiceImpl(val session: Session, autoCreateSchema: Boolean = true) : PollService {
+    val subscriberUDT: UserType
+
     init {
         if (autoCreateSchema) createSchema()
+        subscriberUDT = session.cluster.metadata.getKeyspace("doodle").getUserType("subscriber")
     }
+
     override fun getPollById(pollId: String): Poll? {
-        val row = session.execute("SELECT * FROM doodle.polls WHERE id = ?", UUID.fromString(pollId)).one()
+        val row = session.execute("SELECT * FROM doodle.polls WHERE id = ?", pollId.toUUID()).one()
         return when (row) {
             null -> null
-            else -> {
-                val poll = Poll()
-                poll.id = row.getUUID("id").toString()
-                poll.label = row.getString("label")
-                poll.choices = row.getList("choices", String::class.java)
-                poll.email = row.getString("email")
-                poll.maxChoices = row.getInt("maxChoices")
-                poll.subscribers = row.getList("subscribers", UDTValue::class.java).map {
-                    Subscriber(
-                            label = it.getString("label"),
-                            choices = it.getList("choices", String::class.java)
-                    )
-                }.toMutableList()  // TODO: no mutable ?
-                poll
-            }
+            else -> rowToPoll(row)
         }
     }
 
@@ -43,15 +35,23 @@ class CassandraPollServiceImpl(val session: Session, autoCreateSchema: Boolean =
     }
 
     override fun addSubscriber(pollId: String, subscriber: Subscriber): Poll {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val udtValue = subscriberUDT.newValue().apply {
+            setString("label", subscriber.label)
+            setList("choices", subscriber.choices)
+        }
+        session.execute(
+                "UPDATE doodle.polls SET subscribers = subscribers + ? WHERE id = ?",
+                listOf(udtValue), pollId.toUUID()
+        )
+        return getPollById(pollId)!!
     }
 
     override fun deletePoll(pollId: String) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        session.execute("DELETE FROM doodle.polls WHERE id = ?", pollId.toUUID())
     }
 
     override val allPolls: List<Poll>
-        get() = throw UnsupportedOperationException()
+        get() = session.execute("SELECT * FROM doodle.polls").map(::rowToPoll)
 
     fun createSchema() {
         with (session) {
@@ -78,5 +78,23 @@ class CassandraPollServiceImpl(val session: Session, autoCreateSchema: Boolean =
                 )
             """)
         }
+    }
+}
+
+private fun String.toUUID() = UUID.fromString(this)
+
+internal fun rowToPoll(row: Row): Poll {
+    return Poll().apply {
+        id = row.getUUID("id").toString()
+        label = row.getString("label")
+        choices = row.getList("choices", String::class.java)
+        email = row.getString("email")
+        maxChoices = row.getInt("maxChoices")
+        subscribers = row.getList("subscribers", UDTValue::class.java).map {
+            Subscriber(
+                    label = it.getString("label"),
+                    choices = it.getList("choices", String::class.java)
+            )
+        }.toMutableList()  // TODO: no mutable ?
     }
 }
