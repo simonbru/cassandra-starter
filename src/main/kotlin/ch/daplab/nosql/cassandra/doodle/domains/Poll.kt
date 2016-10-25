f817960f680e10962c0559209ced50f04b35df80
Simon Brulhart <simon@brulhart.me>
2016-10-25 20:51:26 +0200

    Implement createPoll and getPollById

--- src/main/kotlin/ch/daplab/nosql/cassandra/doodle/domains/Poll.kt
+++ src/main/kotlin/ch/daplab/nosql/cassandra/doodle/domains/Poll.kt
@@ -1,14 +1,13 @@
 package ch.daplab.nosql.cassandra.doodle.domains
 
-class Poll {
-
-    var id: String? = null
-    var label: String? = null
-    var choices: List<String>? = null
-    var email: String? = null
-    var maxChoices: Int? = null
+class Poll(
+    var id: String? = null,
+    var label: String? = null,
+    var choices: List<String> = emptyList(),
+    var email: String? = null,
+    var maxChoices: Int? = null,
     var subscribers: MutableList<Subscriber>? = null
-
+) {
     override fun toString(): String {
         return "Poll [id=$id, label=$label, " +
                 "choices=$choices, email=$email, " +
--- src/main/kotlin/ch/daplab/nosql/cassandra/doodle/services/impl/cassandra/CassandraPollServiceImpl.kt
+++ src/main/kotlin/ch/daplab/nosql/cassandra/doodle/services/impl/cassandra/CassandraPollServiceImpl.kt
@@ -4,17 +4,42 @@ import ch.daplab.nosql.cassandra.doodle.domains.Poll
 import ch.daplab.nosql.cassandra.doodle.domains.Subscriber
 import ch.daplab.nosql.cassandra.doodle.services.PollService
 import com.datastax.driver.core.Session
+import com.datastax.driver.core.UDTValue
+import java.util.*
 
 class CassandraPollServiceImpl(val session: Session, autoCreateSchema: Boolean = true) : PollService {
     init {
         if (autoCreateSchema) createSchema()
     }
     override fun getPollById(pollId: String): Poll? {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val row = session.execute("SELECT * FROM doodle.polls WHERE id = ?", UUID.fromString(pollId)).one()
+        return when (row) {
+            null -> null
+            else -> {
+                val poll = Poll()
+                poll.id = row.getUUID("id").toString()
+                poll.label = row.getString("label")
+                poll.choices = row.getList("choices", String::class.java)
+                poll.email = row.getString("email")
+                poll.maxChoices = row.getInt("maxChoices")
+                poll.subscribers = row.getList("subscribers", UDTValue::class.java).map {
+                    Subscriber(
+                            label = it.getString("label"),
+                            choices = it.getList("choices", String::class.java)
+                    )
+                }.toMutableList()  // TODO: no mutable ?
+                poll
+            }
+        }
     }
 
     override fun createPoll(poll: Poll): Poll {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val uuid = UUID.randomUUID()
+        session.execute(
+                "INSERT INTO doodle.polls (id, label, choices, email, maxChoices) VALUES (?,?,?,?,?)",
+                uuid, poll.label, poll.choices, poll.email, poll.maxChoices
+        )
+        return getPollById(uuid.toString())!!
     }
 
     override fun addSubscriber(pollId: String, subscriber: Subscriber): Poll {
